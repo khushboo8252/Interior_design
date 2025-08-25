6c88fe6e00f78af4897adff3d96f5453937a8fef
khushboo8252 <khushbooranjan8252@gmail.com>
2025-08-25 16:47:51 +0530

    fix: routing

--- frontend/src/router/AppRoutes.jsx
+++ frontend/src/router/AppRoutes.jsx
@@ -64,7 +64,6 @@ function AppRoutes() {
           {/* Privacy */}
           <Route path="/privacy" element={<Privacy />} />
 
-          {/* Recommendations */}
           <Route
             path="/recommendations/designer"
             element={<DesignerRecommendations />}
